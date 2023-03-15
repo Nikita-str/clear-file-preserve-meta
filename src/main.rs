92beebbe2812623be647754d72a6e883d9802e32
Nikita-str <newmynamespace@gmail.com>
2023-03-15 20:31:42 +0300

    `[[bin]]`: add cmd(`-d`) for clear dir

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,10 @@ use clap::Parser;
 struct Cli {
     #[clap(short)]
     /// set of files that need to be clearead while preserving metadata
-    clear: Vec<String>,
+    file_clear: Vec<String>,
+    #[clap(short)]
+    /// set of dirs that need to be clearead while preserving metadata
+    dir_clear: Vec<String>,
     /// new content for cleared files 
     #[clap(short, long, default_value_t={"".into()})]
     new_content: String,
@@ -15,11 +18,17 @@ struct Cli {
 fn main() -> std::io::Result<()> {
     let cli = Cli::parse();
 
-    for file_path in &cli.clear {
+    for file_path in &cli.file_clear {
         if let Err(err) = cl::change_file_content(file_path, &cli.new_content) {
             println!("cant clear file {file_path:?}: {err}")
         }
     }
     
+    for dir_path in &cli.dir_clear {
+        if let Err(err) = cl::change_dir_files_content(dir_path, &cli.new_content, false) {
+            println!("cant clear file {dir_path:?}: {err}")
+        }
+    }
+
     Ok(())
 }
